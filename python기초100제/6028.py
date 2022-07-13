ccbae3b2209f83ca2bbbe3846f98e8592f27a55f
YoonDii <dbswl9506@naver.com>
2022-07-14 01:43:30 +0900

    파이썬문풀

---
+++ python기초100제/6028.py
@@ -0,0 +1,5 @@
+# 10진수를 입력받아 16진수(hexadecimal)로 출력해보자.
+
+n = input()
+n = int(n)
+print('%X'%n)
---
+++ python기초100제/6030.py
@@ -0,0 +1,4 @@
+# 영문자 1개를 입력받아 10진수 유니코드(Unicode) 값으로 출력해보자.
+
+n = ord(input())  #ord( ) 는 어떤 문자의 순서 위치(ordinal position) 값을 의미한다.  
+print(n)
